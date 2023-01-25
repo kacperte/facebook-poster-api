bb2bd8bc4f27a3129bc117a66ae93696a626b92e
Kacper <k.trzepiecinski@gmail.com>
2023-01-26 00:29:06 +0100

    Add comments to code

--- app/db/database.py
+++ app/db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# configuration for connecting to the database
 conf = {
     "host": "db",
     "port": "5432",
@@ -9,15 +10,21 @@ conf = {
     "user": "postgres",
     "password": "postgres",
 }
+
+# create the engine for connecting to the database
 engine = create_engine(
     "postgresql://{user}:{password}@{host}:{port}/{database}".format(**conf)
 )
+
+# create a session local factory for creating sessions to the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# create a base class for declarative models
 Base = declarative_base()
 
 
 def get_db():
+    """ Function for getting a database session """
     db = SessionLocal()
     try:
         yield db
