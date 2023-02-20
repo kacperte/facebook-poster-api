1154783ef1a421eb5ded926442de58a29aeac96a
Kacper <k.trzepiecinski@gmail.com>
2023-02-20 21:39:27 +0100

    Add validation for email and groups_name parameters using Pydantic models. This increases security and facilitates testing.

--- app/router/bot.py
+++ app/router/bot.py
@@ -7,35 +7,51 @@ from app.db.database import get_db
 from sqlalchemy.orm.session import Session
 from app.config import SECRET_KEY_HASH
 from app.db.hash import Hash
+from pydantic import BaseModel
+
 
 secret_key = SECRET_KEY_HASH
 
 
 router = APIRouter(prefix="/bot", tags=["bot"])
 
+
+class ContentRequest(BaseModel):
+    login: str
+    password: str
+    email: str
+    groups_name: str
+
+
 @router.post(
     path="/run",
     summary="Run function that send content to Facebook groups",
     description="This API call function that send content to Facebook groups - text and image.",
 )
-async def send_content_to_fb_groups(db: Session = Depends(get_db), email: str = None, groups_name: str = None):
+async def send_content_to_fb_groups(
+    db: Session = Depends(get_db), content_request: ContentRequest = None
+):
 
     response_token = requests.post(
         url="http://localhost:8000/token",
-        data={"grant_type": "password", "username": "kacper", "password": "kacper"},
+        data={
+            "grant_type": "password",
+            "username": content_request.login,
+            "password": content_request.password,
+        },
     ).content
     response_dict = json.loads(response_token)
     auth_token = response_dict["access_token"]
     headers = {"Authorization": f"Bearer {auth_token}"}
 
     response_groups = requests.get(
-        url=f"http://localhost:8000/gropus/group/{groups_name}",
+        url=f"http://localhost:8000/gropus/group/{content_request.groups_name}",
         headers=headers,
     ).content
     response_dict = json.loads(response_groups)
-    groups = response_dict['groups'].split(',')
+    groups = response_dict["groups"].split(",")
 
-    password = db_user.get_user(db, email).password
+    password = db_user.get_user(db, content_request.email).password
     enc_pass = Hash(secret_key).decrypt_password(password)
 
-    facebook_poster(login=email, password=enc_pass, groups=groups)
+    facebook_poster(login=content_request.email, password=enc_pass, groups=groups)
