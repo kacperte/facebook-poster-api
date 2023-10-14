67c2075fdb96506aa72fe8bd17c15070e660d850
Kacper <k.trzepiecinski@gmail.com>
2023-10-15 00:26:56 +0200

    Improve error handling and code readability in execute_daily_tasks
    - Enhanced error handling for spreadsheet authorization and API responses.
    - Introduced constants for column and row values to enhance code clarity.
    - Renamed client variable to avoid naming conflict.
    - Checked for empty API response content.

--- app/crone_jobs/task_executer.py
+++ app/crone_jobs/task_executer.py
@@ -9,6 +9,14 @@ import base64
 import os
 
 
+COLS_PER_DAY = 5
+COL_OFFSET_RECRUITER = 2
+COL_OFFSET_MATERIAL_ID = 3
+COL_OFFSET_GROUPS_NAME = 4
+COL_OFFSET_TASK_STATUS = 5
+FIRST_ROW = 3
+LAST_ROW = 11
+
 SCOPE = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,15 +29,16 @@ URL = os.environ.get("HOST_IP")
 
 
 def make_api_request(url, headers=None, method="GET", **kwargs):
-    """Make an API request and return the JSON response."""
     try:
         response = requests.request(method, url, headers=headers, **kwargs)
         response.raise_for_status()
+
+        if not response.content:
+            raise ValueError("Empty response from API")
+
         return json.loads(response.content)
     except requests.HTTPError as e:
-        raise ValueError(
-            f"Invalid credentials, HTTP status code: {e.response.status_code}"
-        )
+        raise ValueError(f"HTTP status code: {e.response.status_code}")
 
 
 def get_secret_value(secret_name, namespace, key):
@@ -37,7 +46,6 @@ def get_secret_value(secret_name, namespace, key):
     v1 = client.CoreV1Api()
 
     secret = v1.read_namespaced_secret(secret_name, namespace)
-
     encoded_value = secret.data.get(key)
     if encoded_value:
         decoded_value = base64.b64decode(encoded_value).decode("utf-8")
@@ -48,29 +56,30 @@ def get_secret_value(secret_name, namespace, key):
 
 
 def execute_daily_tasks():
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(PATH, SCOPE)
-    client = gspread.authorize(credentials)
-    spreadsheet = client.open_by_url(url=FILE_URL)
+    try:
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(PATH, SCOPE)
+        client_gspread = gspread.authorize(credentials)
+        spreadsheet = client_gspread.open_by_url(url=FILE_URL)
+    except Exception as e:
+        print(f"Problem z autoryzacją do arkusza kalkulacyjnego: {e}")
+        return
+
     worksheet = spreadsheet.get_worksheet(2)
 
     current_day = datetime.datetime.today().weekday()
-    col_with_recruiter_login = (current_day * 5) + 2
-    col_with_material_id = (current_day * 5) + 3
-    col_with_groups_name = (current_day * 5) + 4
-    col_with_task_status = (current_day * 5) + 5
-    first_row = 3
-    last_row = 11
+    col_with_recruiter_login = (current_day * COLS_PER_DAY) + COL_OFFSET_RECRUITER
+    col_with_material_id = (current_day * COLS_PER_DAY) + COL_OFFSET_MATERIAL_ID
+    col_with_groups_name = (current_day * COLS_PER_DAY) + COL_OFFSET_GROUPS_NAME
+    col_with_task_status = (current_day * COLS_PER_DAY) + COL_OFFSET_TASK_STATUS
 
-    for row in range(first_row, last_row):
+    for row in range(FIRST_ROW, LAST_ROW):
         if worksheet.cell(row, col_with_task_status).value == "FALSE":
             data = {
                 "login": worksheet.cell(row, col_with_recruiter_login).value,
                 "password": get_secret_value(
                     secret_name="crudentials-secrets",
                     namespace="default",
-                    key=worksheet.cell(row, col_with_recruiter_login).value.split("@")[
-                        0
-                    ],
+                    key=worksheet.cell(row, col_with_recruiter_login).value.split("@")[0],
                 ),
                 "email": worksheet.cell(row, col_with_recruiter_login).value,
                 "groups_name": worksheet.cell(row, col_with_groups_name).value,
@@ -78,14 +87,15 @@ def execute_daily_tasks():
             }
 
             api_endpoint = urljoin(URL, "bot/run")
-            response_dict = make_api_request(api_endpoint, method="POST", json=data)
-            if response_dict:
-                print(f"Zostało uruchomione nowe zadanie do wykonania.")
-            else:
-                return
+            try:
+                response_dict = make_api_request(api_endpoint, method="POST", json=data)
+                if response_dict:
+                    print(f"Zostało uruchomione nowe zadanie do wykonania.")
+            except ValueError as e:
+                print(f"Błąd podczas żądania API: {e}")
 
             worksheet.update_cell(row=row, col=col_with_task_status, value="TRUE")
 
-            return
 
-execute_daily_tasks()
+if __name__ == "__main__":
+    execute_daily_tasks()
