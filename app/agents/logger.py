27d535fcfcd00e2bdb01937a3f4e7e0f1d3e0f74
Kacper <k.trzepiecinski@gmail.com>
2023-10-15 10:35:20 +0200

    Add custom tag to logger output
    - Modified create_logger function to accept an additional tag parameter.
    - Introduced a ContextFilter class to inject the tag into logging records.
    - Updated the logging formatter to display the tag in the log output.

--- app/agents/logger.py
+++ app/agents/logger.py
@@ -1,11 +1,21 @@
 import logging
 
 
-def create_logger(name: str, level=logging.INFO) -> logging.Logger:
+def create_logger(name: str, level=logging.INFO, tag="fb-poster") -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+    formatter = logging.Formatter("%(asctime)s [%(tag)s] %(name)s %(levelname)s: %(message)s")
+
+    # Tworzenie niestandardowego filtra dodającego tag do logów
+    class ContextFilter(logging.Filter):
+        def filter(self, record):
+            record.tag = tag
+            return True
+
+    log_filter = ContextFilter()
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
+    console_handler.addFilter(log_filter)  # Dodawanie filtra do handlera
     logger.addHandler(console_handler)
     return logger
+
